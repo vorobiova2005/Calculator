aea710476a4ead627ba1c8d7bc99e8d1e9670ae1
vorobiova2005 <vorobyova.valentina2005@gmail.com>
2020-04-03 13:55:22 +0300

    Add main.js

---
+++ main.js
@@ -0,0 +1,54 @@
+function plus() {
+  var num1, num2, result;
+
+  num1 = document.getElementById("inpnt1").value;
+  num1 = parseInt(num1);
+
+  num2 = document.getElementById("inpnt2").value;
+  num2 = parseInt(num2);
+
+  result = num1 + num2;
+
+  document.getElementById("out").innerHTML = result;
+}
+
+function minus() {
+  var num1, num2, result;
+
+  num1 = document.getElementById("inpnt1").value;
+  num1 = parseInt(num1);
+
+  num2 = document.getElementById("inpnt2").value;
+  num2 = parseInt(num2);
+
+  result = num1 - num2;
+
+  document.getElementById("out").innerHTML = result;
+}
+
+function multiply() {
+  var num1, num2, result;
+
+  num1 = document.getElementById("inpnt1").value;
+  num1 = parseInt(num1);
+
+  num2 = document.getElementById("inpnt2").value;
+  num2 = parseInt(num2);
+
+  result = num1 * num2;
+
+  document.getElementById("out").innerHTML = result;
+}
+function division() {
+  var num1, num2, result;
+
+  num1 = document.getElementById("inpnt1").value;
+  num1 = parseInt(num1);
+
+  num2 = document.getElementById("inpnt2").value;
+  num2 = parseInt(num2);
+
+  result = num1 / num2;
+
+  document.getElementById("out").innerHTML = result;
+}
